757ff8d9690d7a29bedf37e2a3f4c63ccfaaa38e
Gerson-18 <gerson28enero@gmail.com>
2024-12-07 17:50:46 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,3 @@
+// Simular información dinámica
+document.getElementById("result-amount").textContent = "50"; // Cambiar monto
+document.getElementById("result-name").textContent = "Dominga Querevalu Eca"; // Cambiar nombre
